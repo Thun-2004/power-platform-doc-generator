26a9f63d1e8840191f0139daedd2b58a9121ebe5
Finlay Murphy <2765063m@student.gla.ac.uk>
2025-12-03 12:09:18 +0000

    Commiting the first updates to our ReactApp. Basic, but testing that changes can be made

--- doctor-power/src/App.jsx
+++ doctor-power/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import dpLogo from './assets/dp-logo.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -8,25 +8,21 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='Images'>
+          <img src={dpLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome to doctor Power!</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count +1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        To do:
+        <ul>
+          <li>Add file upload system</li>
+          <li>Get the basic UI done</li>
+        </ul>
       </p>
     </>
   )
