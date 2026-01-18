649d4016c7c293d6dd11cb76ea6af9d4500d8700
Thun-2004 <66011276@kmitl.ac.th>
2026-01-18 01:18:13 +0000

    refactor: rearrange folder structure

---
+++ backend/src/backend.Api/Program.cs
@@ -0,0 +1,77 @@
+using Scalar.AspNetCore;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using backend.Data;
+using backend.Domain;
+using backend.Infrastructure;
+
+namespace backend.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var AllowFrontend = "_myAllowSpecificOrigins"; //cors rule's name
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(name: AllowFrontend,
+                            policy  =>
+                            {
+                                policy.WithOrigins("http://localhost:5173");
+                            });
+        });
+        
+        //NOTE: testing only
+        // var uploadRoot = Path.Combine(
+        //     Environment.GetFolderPath(Environment.SpecialFolder.Desktop), 
+        //     "uploads-test"
+        // ); 
+        // Directory.CreateDirectory(uploadRoot); 
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddInfrastructures();
+        //FIX: in memory cache for testing
+        builder.Services.AddSingleton<IUploadStore, UploadStore>();
+        // builder.Services.AddApplication();
+
+        builder.Services.AddOpenApi();
+
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseInMemoryDatabase("AuthDb"); 
+        }
+        );
+
+        builder.Services.AddAuthorization(); 
+
+        //setup table (in memory)
+        builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<AppDbContext>(); 
+
+        var app = builder.Build();
+
+        //map 
+        app.MapIdentityApi<IdentityUser>(); 
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.MapScalarApiReference();
+
+            // Automatically redirect to Scalar documentation (only in dev)
+            app.MapGet("/", () => Results.Redirect("/scalar")); 
+            
+        }
+
+        app.UseAuthentication();
+        app.UseHttpsRedirection();
+        app.UseCors(AllowFrontend);
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
