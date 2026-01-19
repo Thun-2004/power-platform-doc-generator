8ae70bd1534b799f9713d1c68bb14f749ec8f83a
Thun-2004 <66011276@kmitl.ac.th>
2026-01-19 22:24:45 +0000

    remove duplicated file

--- backend/src/backend.Infrastructure/obj/Debug/net9.0/backend.Infrastructure.AssemblyInfo.cs
+++ backend/src/backend.Infrastructure/obj/Debug/net9.0/backend.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a1aa1b76c72e390b50d6c7d3d5aff2130884b05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c04012c990d61699e2f39c5f1448a0a21721eb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
