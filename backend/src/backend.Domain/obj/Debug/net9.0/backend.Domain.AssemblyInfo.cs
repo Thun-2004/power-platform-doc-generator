8ce610694b931ddfa4a5eefb365882c3fcb1922e
Kyle Smith <2912472s@student.gla.ac.uk>
2026-02-11 11:52:57 +0000

    no idea lil bro

--- backend/src/backend.Domain/obj/Debug/net9.0/backend.Domain.AssemblyInfo.cs
+++ backend/src/backend.Domain/obj/Debug/net9.0/backend.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f939191f7b46ed4154e5b66cf81925eb15af5c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81903a7531bb34bf7702aa09753210b710605392")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
