d69b8fb5c45ab866b1c39af3ff6f20384e260544
Thun-2004 <66011276@kmitl.ac.th>
2026-02-24 21:02:13 +0000

    remove backend folder

--- backend/src/backend.Domain/PermittedExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace backend.Domain; 
-
-public enum PermittedExtensions
-{
-    Unknown = 0,
-    MSApp = 1, 
-    Zip = 2, 
-    Pdf = 3
-}
--- backend/src/backend.Domain/UploadedFiles.cs
+++
@@ -1,9 +0,0 @@
-﻿// using Microsoft.EntityFrameworkCore.Metadata.Internal;
-
-namespace backend.Domain;
-
-public class UploadedFile
-{
-    public required string OriginalName { get; set; }
-    public required string StoredPath { get; set; }
-}
