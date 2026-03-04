530ea5a82e996d499adc42ee6f605be0c6464733
Thun-2004 <66011276@kmitl.ac.th>
2026-03-04 08:00:51 +0000

    merge local branch bugfix/0-screen

--- backend/src/backend.Api/DTO/JobStatusResponse 2.cs
+++
@@ -1,9 +0,0 @@
-namespace backend.Api.DTO; 
-
-
-public class StatusResponse 
-{
-    public required string JobId { get; set; }
-    public required string JobStatus { get; set; }
-    public required Dictionary<string, string> Progress { get; set; }
-}
--- backend/src/backend.Api/DTO/UploadRequest 2.cs
+++
@@ -1,8 +0,0 @@
-
-namespace backend.Api.DTO; 
-
-public class UploadRequest
-{
-    public IFormFile File { get; set; } = default!;
-    public List<string> SelectedOutputTypes { get; set; } = new();
-}
