046fe58b53121944ade2bafce1eb7bcf476453f7
Thun-2004 <66011276@kmitl.ac.th>
2026-03-04 20:29:28 +0000

    WIP: return error when generating file fails

--- backend/src/backend.Api/DTO/JobStatusResponse.cs
+++ backend/src/backend.Api/DTO/JobStatusResponse.cs
@@ -6,4 +6,6 @@ public class StatusResponse
     public required string JobId { get; set; }
     public required string JobStatus { get; set; }
     public required Dictionary<string, string> Progress { get; set; }
+    string? Error,
+    List<string>? FailedOutputType
 }
