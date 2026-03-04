8bf8bbb37271ea16d9986e3231cca58434836498
Thun-2004 <66011276@kmitl.ac.th>
2026-03-04 08:00:51 +0000

    merge local branch bugfix/0-screen

--- backend/src/backend.Api/Db/cache 2.cs
+++
@@ -1,9 +0,0 @@
-using backend.Domain; 
-
-public interface IUploadStore {
-    UploadedFile File { get; }
-}
-
-public class UploadStore : IUploadStore {
-    public UploadedFile File { get; set; } = default!;
-}
