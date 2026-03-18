7d9cfa6f7763e5fb8b63fa26c401272147f79df6
Thun-2004 <66011276@kmitl.ac.th>
2026-03-18 15:01:45 +0000

    feat: add LLM health check upon starting + disable not working model in dropdown

--- backend/src/backend.Api/Controllers/ConfigController.cs
+++ backend/src/backend.Api/Controllers/ConfigController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using backend.Application.Config;
+using backend.Api;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
@@ -31,6 +32,21 @@ public class ConfigController : ControllerBase
             backendUrl = _shared.BackendUrl,
             aiModels = aiModelsFlat
         });
+    }
+
+    [HttpGet("llm-status")]
+    public async Task<IActionResult> GetLlmStatus([FromServices] IServiceProvider services)
+    {
+        var statuses = await LlmHealth.GetStatusesAsync(services);
+
+        return Ok(statuses.Select(s => new
+        {
+            model = s.Model,
+            provider = s.Provider,
+            baseUrl = s.BaseUrl,
+            isHealthy = s.IsHealthy,
+            error = s.Error
+        }));
     }
 }
 
